d57bdf9037ae098f4629c279d29d36f222d49731
matheus-liber <matheuslibergt@gmail.com>
2023-02-20 16:29:57 -0300

    Finalizado curso 3 módulo 1

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
 
     protected elemento:HTMLElement;
@@ -16,6 +18,7 @@ export abstract class View<T> {
         }
     }
 
+    @logarTempoDeExecucao()
     public update(model: T): void {
         let template = this.template(model);
         if (this.escapar) {
@@ -23,6 +26,7 @@ export abstract class View<T> {
         }
         this.elemento.innerHTML = template;
     }
+    
 
     protected abstract template(model: T): string;
 }
