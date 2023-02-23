01942e92aa17ca19f5de048255939b23b8dc4eda
matheus-liber <matheuslibergt@gmail.com>
2023-02-23 14:30:32 -0300

    Módulo 2

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,11 +1,11 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T> {
 
     protected elemento:HTMLElement;
-    private escapar = false
 
-    constructor(seletor: string, escapar?: boolean) {
+    constructor(seletor: string) {
         
         const elemento = document.querySelector(seletor);
         if (elemento) {
@@ -13,17 +13,12 @@ export abstract class View<T> {
         } else {
             throw Error (`Seletor ${seletor} não existe no DOM. Verifique!`);
         }
-        if (escapar) {
-            this.escapar = escapar;
-        }
     }
 
+    @inspect
     @logarTempoDeExecucao()
     public update(model: T): void {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<\/script>/, "");
-        }
         this.elemento.innerHTML = template;
     }
     
